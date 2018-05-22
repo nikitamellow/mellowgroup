69c13949fc14d9949203dd84ef851b1bae192230
Paul White <emberprime43@gmail.com>
2018-05-22 20:11:20 +0300

    what we do circle

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
     
     
     centralize();
+    placeIcons();
+    
     $(window).resize(function(){
         centralize(); 
     });
@@ -40,15 +42,46 @@ $(document).ready(function(){
 
 var centralize = function(){
         
-        let ww = $('.about').width();
-        let wh = $('.about').height();
-        
-        let iw = $('.central>img').width();
-        let ih = $('.central>img').height();
-        
-        $('.central').css('left', Math.round( (ww - iw)/2 ));
-        
-        $('.central').css('top', Math.round( (wh - ih)/2 ));
-        
+    let ww = $('.about').width();
+    let wh = $('.about').height();
+    
+    let iw = $('.central>img').width();
+    let ih = $('.central>img').height();
+    
+    $('.central').css('left', Math.round( (ww - iw)/2 ));
+    
+    $('.central').css('top', Math.round( (wh - ih)/2 ));
+    
+}
+
+var placeIcons = function(){
+    
+    let Ra = 280;
+    let Rb = 380;
+    //let els = Array.from( document.getElementsByClassName('el'));
+    //console.log (els);
+    //let N = els.length;
+    let N = 9;
+    let dfi = 2 * Math.PI / N;
+    console.log(N);
+    console.log(dfi);
+    
+    
+    let center = {};
+    center.x = Math.round( $('.about').width()/2.2 );
+    center.y = Math.round( $('.about').height()/2.2 );
+    console.log(center);
+    
+    let i = 0;
+    for(i=0;i<N;i++){
+        console.log('cycle');
+        $('div.el:nth-of-type('+(i+2)+')').css('left', Math.round(center.x + Rb * Math.sin(dfi*i) ) );
+        $('div.el:nth-of-type('+(i+2)+')').css('top', Math.round(center.y + Ra * Math.cos(dfi*i) ) );
+        /*console.log (els[i].style.left);
+        els[i].style.left = toString(Math.round( center.x + R * Math.sin(dfi*i) ))+'px';
+        els[i].style.top = toString(Math.round( center.y + R * Math.cos(dfi*i) ))+'px';
+        console.log (els[i].style.left);*/
     }
+    //console.log (els);
+}
 
